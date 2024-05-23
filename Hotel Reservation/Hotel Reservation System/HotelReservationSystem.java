572bcc1e0f2f3f6591bbb33ff19c46921ebcc5d6
Janhavi <98639253+jks1111@users.noreply.github.com>
2024-05-24 03:04:32 +0530

    Update HotelReservationSystem.java

--- Hotel Reservation/Hotel Reservation System/HotelReservationSystem.java
+++ Hotel Reservation/Hotel Reservation System/HotelReservationSystem.java
@@ -1,26 +1,3 @@
-//import java.sql.DriverManager;
-//import  java.sql.SQLException;
-//import  java.sql.Connection;
-//import  java.util.Scanner;
-//import  java.sql.Statement;
-//import java.sql.ResultSet;
-//
-//public class HotelReservationSystem {
-//    private  static  final String url = "jdbc:mysql://localhost:3306/hotel_db";
-//    private static final String username = "root";
-//    private  static  final String password = "Jks@KD11";
-//
-//    public static void main(String[] args)  throws ClassNotFoundException, SQLException{
-//               try{
-//                   Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//               }catch (ClassNotFoundException e){
-//                   System.out.println(e.getMessage());
-//               }
-//
-//    }
-//}
-//
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
